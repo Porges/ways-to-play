b8553ab02d464caa219bcfb656651cc1e06c7f7a
George Pollard <porges@porg.es>
2020-02-11 14:47:55 +1300

    Redo home screen

--- src/content/Home.tsx
+++ src/content/Home.tsx
@@ -2,22 +2,27 @@ import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
 
 import { Link } from 'react-router-dom';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export const Home: React.FC = () => {
   return (<>
     <Helmet>
       <body itemScope itemType="http://schema.org/WebSite" />
     </Helmet>
-
-    <h1>Ways to Play</h1>
-    <p>
-      There are a few things here:
-    </p>
-    <ul>
-      <li><Link to="/about">About</Link></li>
-      <li><Link to="/articles">Articles</Link></li>
-      <li><Link to="/games">Games</Link></li>
-      <li><Link to="/bibliography">Bibliography</Link></li>
-    </ul>
+    <h1>Welcome</h1>
+    <Row>
+      <Col/>
+      <Col xs="10" lg="6">
+        <p className="text-center">
+            Welcome to <cite>Ways to Play</cite>. 
+            This is a site about games, traditional and modern, that are played around the world. There are two main areas on this site, of <Link to="/articles"><span role="img" aria-label="">🧾</span>&nbsp;Articles</Link> about games or families of games, and <Link to="/games"><span role="img" aria-label="">🎲</span>&nbsp;Games</Link> for rules of the games themselves.
+        </p>
+        <p className="text-center">
+          You might also be interested in the <Link to="/about"><span role="img" aria-label="">📣</span>&nbsp;About</Link> page or perhaps the site-wide <Link to="/bibliography"><span role="img" aria-label="">📚</span>&nbsp;Bibliography</Link>.
+        </p>
+      </Col>
+      <Col/>
+    </Row>
   </>);
 }
