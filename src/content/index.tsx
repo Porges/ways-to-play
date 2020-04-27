08963fa200739e3808510a3993dc8900f37ca81b
George Pollard <porges@porg.es>
2020-04-27 14:02:12 +1200

    Add 404 handler

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -5,8 +5,17 @@ import { About } from './About';
 import { Home } from './Home';
 import { Games } from './games';
 import { Articles } from './articles';
+import { Helmet } from 'react-helmet-async';
 
 const Bibliography = React.lazy(() => import(/* webpackChunkName: 'bibliography' */ './Bibliography'))
+
+const FourOhFour: React.FC = () => (<>
+    <Helmet>
+        <title>404 Not Found</title>
+    </Helmet>
+    <h1>404 Not Found</h1>
+    <p>Nothing was found at this location.</p>
+</>);
 
 export const Routes: React.FC = () => {
     return (
@@ -17,6 +26,7 @@ export const Routes: React.FC = () => {
                 <Route path="/games" component={Games} />
                 <Route path="/articles" component={Articles} />
                 <Route path="/bibliography" exact component={Bibliography} />
+                <Route component={FourOhFour}/>
             </Switch>
         </React.Suspense>
     );
