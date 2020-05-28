5f8eb31a1f3a5d504363c23e59a6273eaff546df
George Pollard <porges@porg.es>
2020-05-28 21:41:25 +1200

    Update About page to be less pretentious

--- src/content/About.tsx
+++ src/content/About.tsx
@@ -20,7 +20,7 @@ export const About: React.FC = () => (
             <blockquote>
             <p>In the introductory remarks to her encyclopedic <em>All Cards on the Table</em>, renowned card historian Sylvia Mann talked about researching playing cards as “a limitless source of instruction and pleasure,” her “way into history.” She remarked (1990:Vol. I:9, 10), “Personally, I have acquired, through application and countless reference works and the talents of other collectors, some knowledge about a lot of subjects hitherto outside my interests…” This was equally true for the Waylands for four decades, and then for me, too. We pursued with enthusiasm a host of “peripheral” topics to ensure a complete understanding of the context of card playing in the New World: Copey trees, paper-making in Mexico, laws prohibiting gambling, how to prepare rawhide, the New World origin of Monte, how to date beads and buttons, the history of clothing in northern Mexico, Native American painting techniques and preparation of pigments, World’s Fairs, Papago place names, scanning electron microscope examinations of stubs of hairs on cards, archaeological excavations in New Mexico, Araucanian painted guanaco-hide cloaks from Chile, Santa Anita Race Track, John Wayne movies, and thumbnail sketches of a seemingly endless list of museum curators and U.S. Army personnel.</p>
             </blockquote>
-            <p>… have you read W. G. Sebald’s <cite>Rings of Saturn</cite>? Games are a foothold into other cultures and other times.</p>
+            <p>I was somewhat inspired by (but make no claim to approaching) W. G. Sebald’s <cite>Rings of Saturn</cite>; games are a foothold for climbing inside other cultures and other times.</p>
             <h2 id="a-note-on-languages">A note on languages</h2>
             <p>Where possible I have tried to supply terms in their original languages, in preference to the romanized forms found in most books. The main reason for doing this is that it’s possible to search using the original terms and find references from the people who are actually playing these games.</p>
             <p>However, since I am not fluent in any language other than English, this has been difficult. If you have any corrections to supply then I am happy to update them.</p>
@@ -38,7 +38,7 @@ export const About: React.FC = () => (
             <p><a href="https://wakufactory.jp/densho/font/hentai/">UniHentaiKana</a> is used as a fallback font for <span lang="ja-Latn">hentaigana</span> characters. It is licensed under the <a href="https://opensource.org/licenses/IPA">IPA Font License</a>.</p>
             <p>A modified version of the <a href="https://cc-icons.github.io/">Creative Commons Web Font</a> (1.2.0) by Ricardo Barros is used to indicate licensing information. Both the font and my modifications are licensed under the <a href="https://creativecommons.org/licenses/by/4.0/">CC 4.0 BY</a> license.</p>
             <h2 id="colophon">Colophon</h2>
-            <p>The site is generated with React, using react-snap to output HTML pages. The fonts are generally from <a href="https://www.typography.com/">Hoefler &amp; Co.</a> (Mercury, Gotham, Archer, Deuce).</p>
+            <p>The site is generated with React, using react-snap to output HTML pages. The fonts are by Lora by Cyreal (body) &amp; Quicksand by Andrew Paglinawan (headings).</p>
             </article>
         </Col>
         <Col/>
