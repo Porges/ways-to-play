0857624d1972b3e0581f0a1d4044fc471d3c8f32
George Pollard <porges@porg.es>
2020-06-27 16:36:38 +1200

    Correct ouro → ouru

--- src/content/games/Kakkuri/index.tsx
+++ src/content/games/Kakkuri/index.tsx
@@ -38,23 +38,23 @@ const Kakkuri: React.FC = () => {
       The 3 of <span lang="ja-Latn">pao</span>, also known as <span lang="ja">黒火箸</span> <span lang="ja-Latn">kuro-hibashi</span> ‘black fire tongs’.
       </ArticleImage>
     <p>Before looking at their cards, each player in turn can exchange their hand with the dealer’s hand. This can happen multiple times so that players can end up with the hands of previous players if multiple players swapped.</p>
-    <p>Next, the players look at their cards. If any player has been dealt the non-red <Cards>1</Cards>s of <span lang="ja-Latn">isu</span>, <span lang="ja-Latn">ouro</span>, and <span lang="ja-Latn">kotsu</span> (or <Cards>AsAdAh</Cards> if playing with standard playing cards), then they win instantly and are paid 3× by each other player. This is called <span lang="ja">ウタズ</span> (from <span lang="ja">打たず</span> <span lang="ja-Latn">utazu</span>, “without playing”).</p>
+    <p>Next, the players look at their cards. If any player has been dealt the non-red <Cards>1</Cards>s of <span lang="ja-Latn">isu</span>, <span lang="ja-Latn">ouru</span>, and <span lang="ja-Latn">kotsu</span> (or <Cards>AsAdAh</Cards> if playing with standard playing cards), then they win instantly and are paid 3× by each other player. This is called <span lang="ja">ウタズ</span> (from <span lang="ja">打たず</span> <span lang="ja-Latn">utazu</span>, “without playing”).</p>
     <ArticleImage
       position="small"
       src={imgUtazu}
       alt="">
-      The <Cards>1</Cards>s of <span lang="ja-Latn">isu</span>, <span lang="ja-Latn">ouro</span>, and <span lang="ja-Latn">kotsu</span>.
+      The <Cards>1</Cards>s of <span lang="ja-Latn">isu</span>, <span lang="ja-Latn">ouru</span>, and <span lang="ja-Latn">kotsu</span>.
     </ArticleImage>
   </Section>
   <Section title="Play">
     <p>To start play, the dealer turns up the top card of the box, which will count as the first card played. The dealer then takes the first turn.</p>
     <p>On a player’s turn they may play several cards. Each card must be one rank higher than the last card played (e.g. if the last card is a <Cards>3</Cards>, you can only play a <Cards>4</Cards>). However, a <Cards>1</Cards> can be played after a <Cards>1</Cards>, and a <Cards>2</Cards> can be played after a <Cards>2</Cards>. When the last card played is a <Cards>12</Cards> <span lang="ja-Latn">kiri </span>(or <Cards>K</Cards>), then any rank can be played next. Suits do not matter, and cards are played on the table in front of the person playing them, rather than in the middle of the table (this makes it easier to see how many cards each player has remaining).</p>
-    <p>In addition, the <Cards>1</Cards> of <span lang="ja-Latn">pao</span> (<Cards>Ac</Cards>) and <Cards>2</Cards> of <span lang="ja-Latn">ouro</span> (<Cards>2d</Cards>) are wild and can be played as any other card. If turned up from the box they count as a normal <Cards>1</Cards> &amp; <Cards>2</Cards>.</p>
+    <p>In addition, the <Cards>1</Cards> of <span lang="ja-Latn">pao</span> (<Cards>Ac</Cards>) and <Cards>2</Cards> of <span lang="ja-Latn">ouru</span> (<Cards>2d</Cards>) are wild and can be played as any other card. If turned up from the box they count as a normal <Cards>1</Cards> &amp; <Cards>2</Cards>.</p>
     <ArticleImage
       position="small"
       src={imgWilds}
       alt="">
-      The wild cards: <Cards>1</Cards> of <span lang="ja-Latn">pao</span> and <Cards>2</Cards> of <span lang="ja-Latn">ouro</span>.
+      The wild cards: <Cards>1</Cards> of <span lang="ja-Latn">pao</span> and <Cards>2</Cards> of <span lang="ja-Latn">ouru</span>.
     </ArticleImage>
     <p>There are two other special cases that allow more than one card of the same rank to be played:</p>
     <ul>
