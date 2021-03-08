1b75076441e2980b68ac40dc8a800b24a43083b8
George Pollard <porges@porg.es>
2021-03-08 13:16:55 +1300

    Add more NZ cards

--- src/content/articles/cards/nz/new-zealand.tsx
+++ src/content/articles/cards/nz/new-zealand.tsx
@@ -8,9 +8,11 @@ import { Link } from 'react-router-dom';
 import imgCroxleyA1 from './Croxley-A-1.jpg';
 import imgCroxleyA2 from './Croxley-A-2.jpg';
 import imgCroxleyA3 from './Croxley-A-3.jpg';
+import imgCroxleyA4 from './Croxley-A-4.jpg';
 import imgCroxleyJ1 from './Croxley-J-1.jpg';
 import imgCroxleyJ2 from './Croxley-J-2.jpg';
 import imgCroxleyJ3 from './Croxley-J-3.jpg';
+import imgCroxleyJ4 from './Croxley-J-4.jpg';
 import imgCroxleyPictorialJ from './Croxley-pictorial-J.jpg';
 import imgCroxleyPictorialJoker from './Croxley-pictorial-joker.jpg';
 import imgCroxleyPictorialBack from './Croxley-pictorial-back.jpg';
@@ -21,6 +23,11 @@ import imgCroxleyCook from './Croxley-Cook.jpg';
 import imgCroxleyQueenstown from './Croxley-Queenstown.jpg';
 import imgCroxleyJoker from './Croxley-Joker.jpg';
 
+import imgTannerCouchAce from './TannerCouch-A.jpg';
+import imgTannerCouchJoker from './TannerCouch-Joker.jpg';
+import imgStrongReadyAce from './StrongReady-A.jpg';
+import imgStrongReadyJoker from './StrongReady-Joker.jpg';
+
 import imgWillisAce from './Willis-Ace.jpg';
 import imgWillisJoker from './Willis-Joker.jpg';
 import imgWillisQueen from './Willis-Queen.jpg';
@@ -30,6 +37,9 @@ import imgUEBQueen from './UEB-Queen.jpg';
 import imgUEB2Back from './UEB2-Back.jpg';
 import imgUEB2Joker from './UEB2-Joker.jpg';
 import imgUEB2Queen from './UEB2-Queen.jpg';
+import imgAmcorAce from './Amcor-Ace.jpg';
+import imgAmcorJoker from './Amcor-Joker.jpg';
+import imgAmcorQueen from './Amcor-Queen.jpg';
 
 import imgGoodallEgmont from './Goodall-Egmont.jpg';
 import imgGoodallMatheson from './Goodall-Matheson.jpg';
@@ -49,7 +59,7 @@ const NZ: React.FC = () =>
         <p>The aces have changed over the years to match the name of the company; the later aces are often poor copies of the earlier ones, where detail has been lost.</p>
         <div className="multi wide">
           <ArticleImage noborder alt="" src={imgCroxleyA1}>
-            An early “John Dickinson & Co.” Ace.
+            An early “John Dickinson & Co.” Realm Ace.
           </ArticleImage>
           <ArticleImage noborder alt="" src={imgCroxleyA2}>
             A “John Dickinson” Ace.
@@ -57,17 +67,23 @@ const NZ: React.FC = () =>
           <ArticleImage noborder alt="" src={imgCroxleyA3}>
             A “DRG” Ace.
           </ArticleImage>
+          <ArticleImage noborder alt="" src={imgCroxleyA4}>
+            A “Croxley” Realm Ace, purchased 2021.
+          </ArticleImage>
         </div>
         <p>The earlier (pre-DRG) cards are most easily identified by a <Cards>J</Cards> index which has a turned-in tail, but this feature is lost in the DRG cards which shifted to a sans-serif index.</p>
         <div className="multi wide">
           <ArticleImage noborder alt="" src={imgCroxleyJ1}>
-            An early “John Dickinson & Co.” Jack, printed in four colours.
+            An early “John Dickinson & Co.” Realm Jack, printed in four colours.
           </ArticleImage>
           <ArticleImage noborder alt="" src={imgCroxleyJ2}>
             A “John Dickinson” Jack, printed in two colours.
           </ArticleImage>
           <ArticleImage noborder alt="" src={imgCroxleyJ3}>
-            A “DRG” Jack, with visible degradation of linework and sans-serif indices.
+            A “DRG” Jack, with portrait reduced in size and sans-serif indices. There is visible degradation of the linework.
+          </ArticleImage>
+          <ArticleImage noborder alt="" src={imgCroxleyJ4}>
+            A “Croxley” Realm Jack, with courts no longer derived from the John Dickinson pattern.
           </ArticleImage>
         </div>
         <p>The curled-<Cards>J</Cards> index is useful in identifying tourist pictorial decks where the card images are replaced by photographs of scenic locations, so that the Ace gives no identifying information. The following cards are from a deck produced before 1958:</p>
@@ -82,7 +98,7 @@ const NZ: React.FC = () =>
             The joker of the deck, featuring a <a href="https://en.wikipedia.org/wiki/Hei-tiki"><span lang="mi">hei-tiki</span></a>.
           </ArticleImage>
         </div>
-        <p>The cards are proabably most often found with backs depicting tourist spots around New Zealand.</p>
+        <p>The cards are probably most often found with backs depicting tourist spots around New Zealand.</p>
         <div className="multi">
           <ArticleImage noborder alt="" src={imgCroxleyTui}>
             Croxley/John Dickinson & Co. card back showing a <a href="https://en.wikipedia.org/wiki/Tui_(bird)">Tūī</a>.
@@ -105,7 +121,7 @@ const NZ: React.FC = () =>
       </Section>
     </Section>
     <Section title="A. D. Willis/United Empire Box">
-      <p>A. D. Willis was a very early manufacturer of playing cards in New Zealand, operating from the 1880s. His designs were eventually taken over by United Empire Box (UEB), some time in the 1960s.</p>
+      <p>A. D. Willis was a very early manufacturer of playing cards in New Zealand, operating from the 1880s. His designs were eventually taken over by United Empire Box (UEB), some time in the 1960s. In the late 1980s the design was taken over by Kiwi Packaging (part of Amcor), and sold under the “Arcade” brand name. This line of designs now appears to be out-of-print, as Amcor no longer manufacture playing cards.</p>
       <ArticleImage
         size="wide"
         src={[
@@ -124,6 +140,15 @@ const NZ: React.FC = () =>
         ]}>
         Cards in the Willis/UEB style, this deck probably made by UEB. This deck is unusual in that the paper is uncoated and the cards feel like plain cardboard.
       </ArticleImage>
+      <ArticleImage
+        size="wide"
+        src={[
+          [imgAmcorAce, ""],
+          [imgAmcorQueen, ""],
+          [imgAmcorJoker, ""],
+        ]}>
+        Cards in the Willis/UEB style, produced by Amcor Cartons.
+      </ArticleImage>
       <p>UEB also produced pictorial souvenir decks, such as the one below.</p>
       <ArticleImage
         size="wide"
@@ -134,6 +159,16 @@ const NZ: React.FC = () =>
         ]}>
         Back and cards from a UEB tourist deck.
       </ArticleImage>
+    </Section>
+    <Section title="Strong & Ready/Tanner Couch">
+        <div className="multi wide">
+          <ArticleImage noborder src={[[imgStrongReadyJoker, ""],[imgStrongReadyAce, ""]]}>
+            Strong & Ready Joker and Ace, from a “Mark 1 Canasta” deck.
+          </ArticleImage>
+          <ArticleImage noborder alt="" src={[[imgTannerCouchAce, ""],[imgTannerCouchJoker, ""]]}>
+            Tanner Couch Ace and Joker, from a “Royal Flush 500” deck.
+          </ArticleImage>
+        </div>
     </Section>
     <Section title="Foreign manufacturers">
       <p>In addition to cards manufactured in New Zealand, cards featuring New Zealand scenes were manufactured in other countries, such as the United Kingdom.</p>
