4163c4cd9644f8b4e7106b29323622b9b2440325
George Pollard <porges@porg.es>
2021-06-15 13:54:27 +1200

    Update About page

--- src/content/About.tsx
+++ src/content/About.tsx
@@ -38,7 +38,8 @@ export const About: React.FC = () => (
             <p>A modified version of the <a href="https://cc-icons.github.io/">Creative Commons Web Font</a> (1.2.0) by Ricardo Barros is used to indicate licensing information. Both the font and my modifications are licensed under the <a href="https://creativecommons.org/licenses/by/4.0/">CC 4.0 BY</a> license.</p>
             <p>The playing-card font is <a href="https://umihotaru.work/">NKD04 by Umihotaru</a>. It has a <a href="https://umihotaru.work/faq.txt">nonstandard but liberal license</a>.</p>
             <h2 id="colophon">Colophon</h2>
-            <p>The site is generated with React, using react-snap to output HTML pages. The fonts are by Lora by Cyreal (body) &amp; Quicksand by Andrew Paglinawan (headings).</p>
+            <p>The site is generated with React, using react-snap to output HTML pages.</p>
+            <p>The fonts are <a href="https://rsms.me/inter/">Inter by Rasmus Andersson</a> (body text), <a href="https://github.com/bBoxType/FiraSans">Fira Sans</a> (body italics), &amp; <a href="http://vollkorn-typeface.com/">Vollkorn by Friedrich Althausen</a> (headings).</p>
             </article>
         </Col>
         <Col/>
