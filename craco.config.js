81adf147efaf84558611fe09139e4fa152611b0c
George Pollard <porges@porg.es>
2021-07-05 02:45:47 +0000

    More caching

--- craco.config.js
+++ craco.config.js
@@ -38,6 +38,7 @@ module.exports = {
                             { adapter: require('responsive-loader/sharp')
                             , sizes: [ 300, 600, 800, 1200, 1600 ]
                             , esModule: true
+                            , cacheDirectory: 'cache-responsive'
                             }
                           }
                         , name: 'static/media/[name]-[width].[hash:8].[ext]'
