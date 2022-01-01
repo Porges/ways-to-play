a9ea3164c42febce51fc6c0b3838572d3d021247
George Pollard <porges@porg.es>
2022-01-01 13:41:48 +1300

    Fixup text a little

--- src/content/games/Teeko/Teeko.tsx
+++ src/content/games/Teeko/Teeko.tsx
@@ -13,8 +13,8 @@ const Teeko: React.FC = () => <>
     <Description>Teeko is a game for two players by John Scarne.</Description>
     <Section title="History">
         <p>Teeko was developed over a period of 15 years by John Scarne.<Cite r="Teeko" /> With the usual Scarne bombast and confidence, he believed that it was a game to rival chess and checkers, and that it would eventually rank alongside (or above) them. Nowadays it is barely known.</p>
-        <p>The game was first published by Scarne in 1955 in <Cite r="Teeko" inline />. This is possibly the only game rulebook ever produced that has had <a href="https://www.quinapalus.com/musical.html">an opera written about it</a>!</p>
-        <p>Scarne thought so much of the game that he named his son (John Teeko Scarne) after the game.<Cite r="AWorldOfGames"/> His (future) wife Steffi Storm is also described in the book as a “top-ranking Teeko player”, although she would later state that “if I win, it’s by dumb luck.”<Cite r="AWorldOfGames" /></p>
+        <p>The game was first published by Scarne in 1955 in the book <Cite r="Teeko" inline />. This is possibly the only game rulebook ever produced that has had <a href="https://www.quinapalus.com/musical.html">an opera written about it</a>!</p>
+        <p>His (future) wife Steffi Storm is described in the book as a “top-ranking Teeko player”, although she would later state that “if I win, it’s by dumb luck.”<Cite r="AWorldOfGames" /> Scarne thought so much of Teeko that he would later name their son (John Teeko Scarne) after it.<Cite r="AWorldOfGames"/></p>
     </Section>
     <Section title="Equipment">
         <p>Teeko is played with four pieces per player (usually in red and black), and a special board (although it can also be played on a standard chess/checkers board):</p>
