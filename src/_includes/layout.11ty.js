5082ee5be5fbd73104fd45d7b52dff9aa46eba39
George Pollard <porges@porg.es>
2024-03-04 18:52:40 +0000

    Clarity

--- src/_includes/layout.11ty.js
+++ src/_includes/layout.11ty.js
@@ -114,6 +114,13 @@ exports.render = async function (data) {
       gtag('js', new Date());
       gtag('config', 'G-Z0CH5J6QX3');
     </script>
+    <script type="text/javascript">
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "gzk1ekbi1n");
+    </script>
   </head>
   <body itemscope itemtype="http://schema.org/WebPage"${data.tags?.includes('article')? ' itemref="breadcrumbs"' : ''}>
     <div itemprop="isPartOf" itemscope itemtype="https://schema.org/WebSite">
