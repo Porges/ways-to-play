3cc841092d31f4cca5b1254f66a805c5489b43bf
George Pollard <porges@porg.es>
2024-06-14 02:53:19 +0000

    Fixup atom feed

--- src/atom.11ty.ts
+++ src/atom.11ty.ts
@@ -1,10 +1,15 @@
 import { ifSet } from "../helpers";
 import { Context, Data } from "../types";
+const { JSDOM } = require('jsdom');
 
 export const data = {
     permalink: "/atom.xml",
     eleventyExcludeFromCollections: true,
 };
+
+function escape(str: string): string {
+    return str.replaceAll('&', '&amp;').replaceAll('<', '&lt;');
+}
 
 export function render(this: Context, data: Data) {
     const all = [...data.collections.article, ...data.collections.game].filter((p: any) => p.data.draft !== true);
@@ -21,14 +26,44 @@ export function render(this: Context, data: Data) {
     <email>porges@porg.es</email>
 </author>
 <updated>${latestUpdated}</updated>
-${all.map((p: any) =>
+${all.slice(0, 10).map((p: any) =>
 `<entry>
-    <title ${ifSet(p.data.titleLang, `xml:lang="${p.data.titleLang}"`)} type="html">${p.data.title.replaceAll('&', '&amp;').replaceAll('<', '&lt;')}</title>
+    <title ${ifSet(p.data.titleLang, `xml:lang="${p.data.titleLang}"`)} type="html">${escape(p.data.title)}</title>
     <id>${data.site.url}${p.url}</id>
     <updated>${p.date.toISOString()}</updated>
     ${p.data.tags.map((tag: string) => `<category term="${tag}" />`).join("")}
-    <content type="text/html" src="${data.site.url}${p.url}" />
+    <link href="${data.site.url}${p.url}" />
+    ${summarize(`${data.site.url}${p.url}`, p.content)}
 </entry>`
 ).join("\n")}
 </feed>`;
+}
+
+function summarize(baseUrl: string, content: string): string {
+    const leadMatcher = /<p class="lead">(.*?)<\/p>/s;
+    let excerpt = content.match(leadMatcher)?.[1];
+    if (excerpt) {
+        const frag = new JSDOM(excerpt);
+        const body = frag.window.document.firstChild.lastChild;
+        
+        removeAll(body.getElementsByClassName("footnote"));
+        removeAll(body.getElementsByClassName("citation"));
+        removeAll(body.getElementsByTagName("audio"));
+        const ps: HTMLCollection = body.getElementsByClassName("pronunciation");
+        for (let i = 0; i < ps.length; ++i) {
+            let p = ps.item(i)!;
+            p.removeAttribute("onclick");
+            p.removeAttribute("title");
+        }
+        
+        return `<summary type="html" xml:base="${baseUrl}">${escape(body.innerHTML)}</summary>`;
+    } else {
+        return `<content type="html" xml:base="${baseUrl}">${escape(content)}</content>`;
+    }
+}
+
+function removeAll(coll: HTMLCollection): void {
+    for (let i = coll.length-1; i >= 0; --i) {
+        coll.item(i)!.remove();
+    }
 }
