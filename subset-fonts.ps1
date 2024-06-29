aaab7c0121556622ae235c8444a565ba113e7eac
George Pollard <porges@porg.es>
2024-06-29 06:10:06 +0000

    fonts 2

--- subset-fonts.ps1
+++ subset-fonts.ps1
@@ -7,13 +7,21 @@ pushd input-fonts/Charis_SIL
 
 $fontFamily = "Charis SIL";
 
-$subsets = @{
+$subsets = [ordered]@{
     "Latin" = @(
-        "U+0020-007F", # Basic Latin
-        "U+00A0-00FF", # Latin-1 Supplement
-        "U+2000-206F") # General Punctuation
-    "Greek" = @(
-        "U+0370-03FF") # Greek and Coptic
+        "BasicLatin",
+        "Latin1Supplement",
+        "LatinExtendedA",
+        "CombiningDiacriticalMarks",
+        "GeneralPunctuation")
+    "ChineseTranslit" = @( # Chinese (Pinyin & Jyutping) transliteration
+        "LatinExtendedB",
+        "SuperscriptsandSubscripts")
+    "VietIndic" = @( # Vietnamese and Indic transliteration
+        "LatinExtendedAdditional")
+    "Historical" = @( # Medieval & Egyptological
+        "LatinExtendedD")
+    # Greek not included as the Charis Greek doesn't cover enough of the block
 }
 
 echo $subsets
@@ -21,22 +29,31 @@ echo $subsets
 $css = "../../fonts/charis.css"
 echo "" > $css # blank it
 
+function whitelistFromBlock([string]$block) {
+    $range = [System.Text.Unicode.UnicodeRanges]::$block
+    if ($range -eq $null) {
+        throw "no such block $block"
+    }
+    
+    return "U+$($range.FirstCodePoint.ToString("x4"))-$(($range.FirstCodePoint + $range.Length - 1).ToString("x4"))"
+}
+
 foreach ($subset in $subsets.GetEnumerator()) {
     $name = $subset.Key 
-    $whitelist = $subset.Value -join ','
+    $whitelist = ($subset.Value | %{ whitelistFromBlock($_) }) -join ','
     
     foreach ($file in Get-ChildItem -Filter '*.ttf' -File) {
         $targetFile = Join-Path "../../fonts/charis" ($file.Name -replace "-", "-$name-" -replace ".ttf", ".woff2")
-        pyftsubset $file --unicodes=$whitelist --flavor=woff2 --harfbuzz-repacker --output-file=$targetFile
+        pyftsubset $file --unicodes=$whitelist --flavor=woff2 --harfbuzz-repacker --output-file=$targetFile '--layout-features=*'
         
-        echo "/* $name */" >> $css
+        echo "/* $name ($($subset.Value -join ', ')) */" >> $css
         echo "@font-face {" >> $css
         echo "    font-family: '$fontFamily';" >> $css
         echo "    src: local('$fontFamily'), url('/fonts/charis/$(Split-Path -Leaf $targetFile)') format('woff2');" >> $css
-        if ($file.Name -contains "Italic") {
+        if ($file.Name -like "*Italic*") {
             echo "    font-style: italic;" >> $css
         }
-        if ($file.Name -contains "Bold") {
+        if ($file.Name -like "*Bold*") {
             echo "    font-weight: bold;" >> $css
         }
         echo "    unicode-range: $whitelist;" >> $css
